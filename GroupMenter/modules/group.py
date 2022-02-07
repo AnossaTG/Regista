e929a47a2e7193fc4eb9a0748ce8d7cd7b841b6d
Talhaisteaqq <85591575+AnossaTG@users.noreply.github.com>
2022-02-07 13:39:06 +0300

    Create group.py

---
+++ GroupMenter/modules/group.py
@@ -0,0 +1,9 @@
+__help__ = """
+ ❍ /setgtitle <isim>*:* Grubunuzdaki yeni sohbet başlığını ayarlar.
+ ❍ /setgpic*:* Grup profili resmini ayarlamak için dosyaya veya fotoğrafa yanıt olarak!
+ ❍ /delgpic*:* Yukarıdakiyle aynı, ancak grup profil resmini kaldırmak için.
+ ❍ /setsticker*:* Grup çıkartma seti olarak ayarlamak için bazı çıkartmalara yanıt olarak!
+ ❍ /setdescription <bio>*:* Grupta yeni sohbet açıklamasını ayarlar.
+"""
+
+__mod_name__ = "GROUP"
